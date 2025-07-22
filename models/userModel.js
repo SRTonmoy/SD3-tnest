04ca3f769f5fcabcf3c645021f6abfae8a5e1561
Md. Shahriar Rahman <152016174+SRTonmoy@users.noreply.github.com>
2025-07-22 18:03:39 +0600

    dshboard added>>log in rout udated>>user name updated

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,20 @@
 // Import the mongoose library to work with MongoDB using schemas and models
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define a schema (blueprint) for the user collection using mongoose.Schema
 const Loginschema = new mongoose.Schema({
-    // Define 'name' field: must be a string and is required (cannot be empty)
-    name: { type: String, required: true },
+  // Define 'name' field: must be a string and is required (cannot be empty)
+  role: {
+    type: String,
+    enum: ["Student", "Teacher", "Admin"],
+    required: true,
+  },
+  name: { type: String, required: true },
 
-    email: { type: String, required: true, unique: true },  // ✅ added email
+  email: { type: String, required: true, unique: true }, // ✅ added email
 
-    // Define 'password' field: must be a string and is required
-    password: { type: String, required: true }
+  // Define 'password' field: must be a string and is required
+  password: { type: String, required: true },
 });
 
 // Create a model named 'users' using the schema above
